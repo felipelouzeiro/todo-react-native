66d944ba58f453a4f37b3be42c8bea7de2e19a8c
felipe <fl.louzeiro@gmail.com>
2022-07-27 21:29:42 -0300

    ✨ feat: criando input e botão

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.Body}>
+          <Text>Hello Word!</Text>
+        </View>
+
+        <View style={styles.Form}>
+          <TextInput style={styles.Input}/>
+          <TouchableOpacity style={styles.Button}/>
+        </View>
+      </View>
+      
+    </>
   );
 }
 
@@ -14,7 +22,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginTop: 20,
+  },
+  Body: {
+    flex: 1,
+  },
+  Form: {
+    padding: 0,
+    height: 60,
     justifyContent: 'center',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    paddingTop: 13,
+    borderTopWidth: 1,
+    borderColor: '#eee',
+  },
+  Input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#eee",
+    borderRadius: 4,
+    paddingHorizontal: 18,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderColor: "#eee",
+  },
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#1c6cce",
+    borderRadius: 4,
+    marginLeft: 10,
   },
 });
